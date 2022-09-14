5a8ffbe1420cab7899d700cf9d04337b6ded5e28
ousttrue <ousttrue@gmail.com>
2022-09-14 13:35:05 +0900

    fix linux

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,13 @@
 const std = @import("std");
 const c = @import("c");
 
-fn uvWrite(stream: *anyopaque, msg: []const u8) !void {
+fn uvWrite(req: *c.uv_write_t, stream: *anyopaque, msg: []const u8) !void {
     var buf: c.uv_buf_t = undefined;
     buf.base = @intToPtr(*u8, @ptrToInt(&msg[0]));
     buf.len = @intCast(c_ulong, msg.len);
 
-    var req: c.uv_write_t = undefined;
     _ = c.uv_write(
-        &req,
+        req,
         @ptrCast(*c.uv_stream_t, @alignCast(@alignOf(c.uv_stream_t), stream)),
         &buf,
         1,
@@ -21,12 +20,14 @@ pub fn main() !void {
     var tty: c.uv_tty_t = undefined;
     _ = c.uv_tty_init(loop, &tty, 1, 0);
     _ = c.uv_tty_set_mode(&tty, 0);
+    defer _ = c.uv_tty_reset_mode();
 
     if (c.uv_guess_handle(1) == c.UV_TTY) {
-        try uvWrite(&tty, "¥033[41;37m");
+        var req0: c.uv_write_t = undefined;
+        try uvWrite(&req0, &tty, "\x1B[41;37m");
     }
+    var req1: c.uv_write_t = undefined;
+    try uvWrite(&req1, &tty, "Hello TTY\n");
 
-    try uvWrite(&tty, "Hello TTY¥n");
-    _ = c.uv_tty_reset_mode();
     _ = c.uv_run(loop, c.UV_RUN_DEFAULT);
 }
