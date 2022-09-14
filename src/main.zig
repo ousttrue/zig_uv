de3c5da114dd63fade3e25d72153a2e542834c5d
ousttrue <ousttrue@gmail.com>
2022-09-14 13:16:06 +0900

    uv_tty

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,32 @@
 const std = @import("std");
 const c = @import("c");
 
+fn uvWrite(stream: *anyopaque, msg: []const u8) !void {
+    var buf: c.uv_buf_t = undefined;
+    buf.base = @intToPtr(*u8, @ptrToInt(&msg[0]));
+    buf.len = @intCast(c_ulong, msg.len);
+
+    var req: c.uv_write_t = undefined;
+    _ = c.uv_write(
+        &req,
+        @ptrCast(*c.uv_stream_t, @alignCast(@alignOf(c.uv_stream_t), stream)),
+        &buf,
+        1,
+        null,
+    );
+}
+
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+    const loop = c.uv_default_loop();
+    var tty: c.uv_tty_t = undefined;
+    _ = c.uv_tty_init(loop, &tty, 1, 0);
+    _ = c.uv_tty_set_mode(&tty, 0);
 
-    const loop = try allocator.create(c.uv_loop_t);
-    defer allocator.destroy(loop);
+    if (c.uv_guess_handle(1) == c.UV_TTY) {
+        try uvWrite(&tty, "¥033[41;37m");
+    }
 
-    _ = c.uv_loop_init(loop);
-    defer _ = c.uv_loop_close(loop);
-
-    std.debug.print("Now quitting.\n", .{});
+    try uvWrite(&tty, "Hello TTY¥n");
+    _ = c.uv_tty_reset_mode();
     _ = c.uv_run(loop, c.UV_RUN_DEFAULT);
 }
